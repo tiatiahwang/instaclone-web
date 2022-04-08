0cf5c1aeb42a8587fac4043e8ff9a7dfe46ffb8f
tiahwang44 <tiahwang44@gmail.com>
2022-04-08 17:17:50 +0900

    [NOMAD] frontend setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { useReactiveVar } from '@apollo/client';
+import { ThemeProvider } from 'styled-components';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import NotFound from './screens/NotFound';
+import { isDarkModeVar, isLoggedInVar } from './apollo';
+import { darkTheme, lightTheme } from './styles/theme';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const isDarkMode = useReactiveVar(isDarkModeVar);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
