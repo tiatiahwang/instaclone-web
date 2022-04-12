443c16f0e376ea951a5e39dcce9a4f1febc41512
tiahwang44 <tiahwang44@gmail.com>
2022-04-12 20:57:30 +0900

    login page

--- src/apollo/vars.ts
+++ src/apollo/vars.ts
@@ -1,4 +1,17 @@
 import { makeVar } from '@apollo/client';
 
+const TOKEN = 'token';
+
 export const isLoggedInVar = makeVar(false);
+
+export const logUserIn = (token: string) => {
+  localStorage.setItem(TOKEN, token);
+  isLoggedInVar(true);
+};
+
+export const logUserOut = () => {
+  localStorage.removeItem(TOKEN);
+  window.location.reload();
+};
+
 export const isDarkModeVar = makeVar(false);
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
 import { faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AuthLayout from '../components/auth/AuthLayout';
@@ -8,6 +9,9 @@ import Input from '../components/auth/Input';
 import Seperator from '../components/auth/Seperator';
 import BottomBox from '../components/auth/ButtonBox';
 import PageTitle from '../components/PageTitle';
+import { LoginMutation, useLoginMutation } from '../graphql/generated';
+import FormError from '../components/auth/FormError';
+import { logUserIn } from '../apollo/vars';
 
 const Notification = styled.div`
   color: #2ecc71;
@@ -21,7 +25,42 @@ const GithubLogin = styled.div`
   }
 `;
 
+interface IForm {
+  username: string;
+  password: string;
+  result?: string;
+}
+
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    clearErrors,
+  } = useForm<IForm>({
+    mode: 'onChange',
+  });
+  const onCompleted = (data: LoginMutation) => {
+    const {
+      login: { ok, error, token },
+    } = data;
+    if (!ok) {
+      return setError('result', { message: error! });
+    }
+    if (token) {
+      logUserIn(token);
+    }
+  };
+  const [login, { loading }] = useLoginMutation({ onCompleted });
+  const onSubmitValid = ({ username, password }: IForm) => {
+    if (loading) return;
+    login({ variables: { username, password } });
+  };
+  const clearLoginError = () => {
+    clearErrors('result');
+  };
+
   return (
     <AuthLayout>
       <PageTitle title="로그인" />
@@ -30,10 +69,39 @@ const Login = () => {
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
         <Notification></Notification>
-        <form>
-          <Input name="username" type="text" placeholder="Username" />
-          <Input name="password" type="password" placeholder="Password" />
-          <Button type="submit" value={'로그인'} />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            {...register('username', {
+              required: '필수 입력 사항입니다',
+              minLength: {
+                value: 3,
+                message: '3자 이상 입력해주세요',
+              },
+            })}
+            name="username"
+            type="text"
+            placeholder="Username"
+            onChange={clearLoginError}
+            hasError={Boolean(errors?.username?.message)}
+          />
+          <FormError message={errors?.username?.message} />
+          <Input
+            {...register('password', {
+              required: '필수 입력 사항입니다',
+            })}
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={clearLoginError}
+            hasError={Boolean(errors?.password?.message)}
+          />
+          <FormError message={errors?.password?.message} />
+          <Button
+            type="submit"
+            value={loading ? '로딩중' : '로그인'}
+            disabled={loading}
+          />
+          <FormError message={errors?.result?.message} />
         </form>
         <Seperator />
         <GithubLogin>
