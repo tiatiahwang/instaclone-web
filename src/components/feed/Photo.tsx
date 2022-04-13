cb817868e74ed6dc38adfd78fdd65f765506e404
tiahwang44 <tiahwang44@gmail.com>
2022-04-13 20:28:08 +0900

    [NOMAD] liking photos

--- src/components/feed/Photo.tsx
+++ src/components/feed/Photo.tsx
@@ -1,5 +1,117 @@
-const Photo = () => {
-  return <></>;
+import styled from 'styled-components';
+import {
+  faBookmark,
+  faComment,
+  faPaperPlane,
+  faHeart,
+} from '@fortawesome/free-regular-svg-icons';
+import { faHeart as SolidHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Avatar from '../Avatar';
+import { FatText } from '../shared';
+import { SeeFeedQuery, useToggleLikeMutation } from '../../graphql/generated';
+
+const PhotoContainer = styled.div`
+  max-width: 615px;
+  margin-bottom: 60px;
+  background-color: white;
+  border: 1px solid rgb(219, 219, 219);
+  border-radius: 4px;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgb(239, 239, 239);
+`;
+
+const Username = styled(FatText)`
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.div`
+  img {
+    width: 100%;
+  }
+`;
+
+const PhotoData = styled.div`
+  padding: 12px 15px;
+`;
+
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const PhotoAction = styled.button`
+  all: unset;
+  cursor: pointer;
+  margin-right: 10px;
+  svg {
+    font-size: 20px;
+  }
+`;
+
+const Likes = styled(FatText)`
+  margin-top: 15px;
+  display: block;
+`;
+
+type ArrayElement<ArrayType extends readonly unknown[] | null | undefined> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+interface Props {
+  photo: ArrayElement<SeeFeedQuery['seeFeed']>;
+}
+
+const Photo = ({ photo }: Props) => {
+  const [toggleLikeMutation] = useToggleLikeMutation();
+  return (
+    <PhotoContainer key={photo?.id}>
+      <PhotoHeader>
+        <Avatar size="lg" url={photo?.user?.avatar} />
+        <Username>{photo?.user?.username}</Username>
+      </PhotoHeader>
+      <PhotoFile>
+        <img src={photo?.file} alt="" />
+      </PhotoFile>
+      <PhotoData>
+        <PhotoActions>
+          <div>
+            <PhotoAction
+              onClick={() =>
+                toggleLikeMutation({ variables: { id: photo!.id } })
+              }
+            >
+              <FontAwesomeIcon
+                style={{ color: photo?.isLiked ? 'tomato' : 'inherit' }}
+                icon={photo?.isLiked ? SolidHeart : faHeart}
+              />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faComment} />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </PhotoAction>
+          </div>
+          <PhotoAction>
+            <FontAwesomeIcon icon={faBookmark} />
+          </PhotoAction>
+        </PhotoActions>
+        <Likes>
+          {photo?.likes === 0
+            ? '0 like'
+            : photo?.likes === 1
+            ? '1 like'
+            : `${photo?.likes} likes`}
+        </Likes>
+      </PhotoData>
+    </PhotoContainer>
+  );
 };
 
 export default Photo;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,109 +1,14 @@
-import styled from 'styled-components';
-import {
-  faBookmark,
-  faComment,
-  faHeart,
-  faPaperPlane,
-} from '@fortawesome/free-regular-svg-icons';
-import { faHeart as SolidHeart } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Avatar from '../components/Avatar';
-import { FatText } from '../components/shared';
 import { useSeeFeedQuery } from '../graphql/generated';
 import PageTitle from '../components/PageTitle';
-
-const PhotoContainer = styled.div`
-  max-width: 615px;
-  margin-bottom: 60px;
-  background-color: white;
-  border: 1px solid rgb(219, 219, 219);
-  border-radius: 4px;
-`;
-
-const PhotoHeader = styled.div`
-  padding: 15px;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid rgb(239, 239, 239);
-`;
-
-const Username = styled(FatText)`
-  margin-left: 15px;
-`;
-
-const PhotoFile = styled.div`
-  img {
-    width: 100%;
-  }
-`;
-
-const PhotoData = styled.div`
-  padding: 12px 15px;
-`;
-
-const PhotoActions = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const PhotoAction = styled.button`
-  all: unset;
-  cursor: pointer;
-  margin-right: 10px;
-  svg {
-    font-size: 20px;
-  }
-`;
-
-const Likes = styled(FatText)`
-  margin-top: 15px;
-  display: block;
-`;
+import Photo from '../components/feed/Photo';
 
 const Home = () => {
-  const { data: { seeFeed } = {} } = useSeeFeedQuery();
+  const { data = {} } = useSeeFeedQuery();
   return (
     <>
       <PageTitle title="홈" />
-      {seeFeed?.map((photo) => (
-        <PhotoContainer key={photo?.id}>
-          <PhotoHeader>
-            <Avatar size="lg" url={photo?.user?.avatar} />
-            <Username>{photo?.user?.username}</Username>
-          </PhotoHeader>
-          <PhotoFile>
-            <img src={photo?.file} alt="" />
-          </PhotoFile>
-          <PhotoData>
-            <PhotoActions>
-              <div>
-                <PhotoAction>
-                  <FontAwesomeIcon
-                    style={{ color: photo?.isLiked ? 'tomato' : 'inherit' }}
-                    icon={photo?.isLiked ? SolidHeart : faHeart}
-                  />
-                </PhotoAction>
-                <PhotoAction>
-                  <FontAwesomeIcon icon={faComment} />
-                </PhotoAction>
-                <PhotoAction>
-                  <FontAwesomeIcon icon={faPaperPlane} />
-                </PhotoAction>
-              </div>
-              <PhotoAction>
-                <FontAwesomeIcon icon={faBookmark} />
-              </PhotoAction>
-            </PhotoActions>
-            <Likes>
-              {photo?.likes === 0
-                ? '0 like'
-                : photo?.likes === 1
-                ? '1 like'
-                : `${photo?.likes} likes`}
-            </Likes>
-          </PhotoData>
-        </PhotoContainer>
+      {data?.seeFeed?.map((photo) => (
+        <Photo key={photo?.id} photo={photo} />
       ))}
     </>
   );
